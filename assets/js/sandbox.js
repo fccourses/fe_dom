e03e38f73f80e5a3408714f701e73633a58a8a68
Vladimir <freshcode.yt@gmail.com>
2021-01-22 18:51:08 +0200

    Add closure task solution

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -5,12 +5,40 @@
  */
 function makeCounter() {
   let count = 0; // замкнутая переменная инкапсулирована
+
   return {
     increment() {
       return ++count;
+    },
+    decrement() {
+      return --count;
     },
   };
 }
 
 const counter1 = makeCounter();
 const counter2 = makeCounter();
+
+/* 
+ Напишите функцию createAdder(n), 
+ которая вернёт другую функцию.
+
+ Возвращенная функция должна складывать
+ Получаемый аргумент m с аргументом n возвращщаюей функции.
+
+const adder = createAdder(10);
+adder(10) //20
+adder(50) //70
+adder(100) //170
+
+*/
+
+function createAdder(initialState) {
+  let state = initialState;
+
+  return function (value) {
+    return state = value + state;
+  };
+  
+}
+const adder = createAdder(100);
