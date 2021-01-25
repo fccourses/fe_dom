72f182250835e6ecf25c36874d1f06dc68aa9bbd
Vladimir <freshcode.yt@gmail.com>
2021-01-25 18:55:43 +0200

    Add task solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,29 @@
 'use strict';
 
-
 /* 
 Поменять местами содержимое 
 двух кнопок по наведению.
 */
+const [closeBtn, openBtn] = document.querySelectorAll('button');
 
+const switcher = ({ target }) => {
+  const closeElem = target === closeBtn ? closeBtn : openBtn;
+  const openElem = target === closeBtn ? openBtn : closeBtn;
+
+  const closeText = closeElem.textContent;
+  closeElem.textContent = openElem.textContent;
+  openElem.textContent = closeText;
+
+  closeElem.removeEventListener('mouseenter', switcher);
+  openElem.addEventListener('mouseenter', switcher);
+};
+
+const clickHandler = ({ target: { textContent } }) => {
+  alert(textContent);
+};
+
+closeBtn.addEventListener('mouseenter', switcher);
+openBtn.addEventListener('mouseenter', switcher);
+
+closeBtn.addEventListener('click', clickHandler);
+openBtn.addEventListener('click', clickHandler);
