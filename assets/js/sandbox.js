6f8b5fa884d04494cb6906b2a50f891635706f74
Vladimir <freshcode.yt@gmail.com>
2021-01-26 14:55:22 +0200

    Add Enum example

---
+++ assets/js/sandbox.js
@@ -0,0 +1,10 @@
+const GENDER_LIST = ['female', 'male', 'unknown']; // enum
+
+class Student {
+  constructor(gender) {
+    this.gender = gender; // 'female' || 'male' || 'unknown'
+  }
+  set gender(value) {
+
+  }
+}
