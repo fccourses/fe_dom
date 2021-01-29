3d4d7d2a55b2b0dfae887f1c2d9b72a717f5f2ca
Vladimir <freshcode.yt@gmail.com>
2021-01-29 20:40:02 +0200

    Add attributes to createElement

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const cardContainer = document.getElementById('root');
+/* const cardContainer = document.getElementById('root');
 const cards = data.map((place) => createPlaceCards(place));
 cardContainer.append(...cards);
-
+ */
 function createPlaceCards(place) {
   const p = createElement(
     'p',
@@ -102,12 +102,28 @@ function stringToColour(str) {
  * @param {object} options
  * @param {string[]} options.classNames - css classes
  * @param {function} options.onClick - click handler
+ * @param {object} options.attributes - click handler
  * @param {Node[]} children
  * @return {HTMLElement}
  */
-function createElement(type, { classNames, onClick }, ...children) {
+function createElement(
+  type = 'div',
+  { classNames = [], onClick = null, attributes = {} }={},
+  ...children
+) {
+  /*
+  EXAMPLE OF ATTR OBJECT
+  const attr = {
+    src: 'https://link.com',
+    alt: 'descr',
+    title: 'descr',
+  };
+   */
   const elem = document.createElement(type);
   elem.classList.add(...classNames);
+  for (const [attrName, attrValue] of Object.entries(attributes)) {
+    elem.setAttribute(attrName, attrValue);
+  }
   elem.onclick = onClick;
   elem.append(...children);
   return elem;
