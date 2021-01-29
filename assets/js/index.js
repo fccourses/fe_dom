81480cd1f1b64687f7a7249d6dae4ea17b3f5257
Vladimir <freshcode.yt@gmail.com>
2021-01-29 18:25:19 +0200

    Add simple task solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,58 @@
 'use strict';
+/* TODO list */
+const state = [];
 
 const form = document.getElementById('root-form');
+const list = document.getElementById('root-list');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const {
-    target: { email },
+    target,
+    target: {
+      todo: { value: rawTodoInput },
+    },
   } = e; // target === form;
-  
-  console.dir(email.value);
+
+  const inputValue = rawTodoInput.trim();
+  /* Валидация значения в инпуте при сабмите формы */
+  if (inputValue) {
+    /* При каждом сабмите формы - пушить значение инпута в массив */
+    state.push(inputValue);
+
+    /* Рендерить на странице то, что было введено в форму */
+    const li = createListElement(inputValue);
+    list.append(li);
+  }
+  /* Очищать форму после каждого сабмита */
+  target.reset();
 });
+
+function createListElement(inputValue) {
+  const li = document.createElement('li');
+  const liContent = document.createTextNode(inputValue);
+
+  li.append(liContent, createDeleteButton(deleteHandler.bind(li)));
+  return li;
+}
+
+function createDeleteButton(onDelete) {
+  const btn = document.createElement('button');
+  btn.textContent = 'X';
+  btn.addEventListener('click', onDelete);
+  return btn;
+}
+
+function deleteHandler(e) {
+  this.remove(); // fix 1
+  const index = state.indexOf(e.target.parentNode.firstChild.textContent);
+  state.splice(index, 1);
+}
+
+/*
+   Кнопка удаления у li - удаляет элемент со стрн и
+   его значение из массива 
+
+   1. Удаление li со страницы
+   2. Удаление Значения из массива
+*/
